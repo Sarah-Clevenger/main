b5bd352e0b2adf65b8813c5d497feebb0ded1f8b
Sarah-Clevenger <89268804+Sarah-Clevenger@users.noreply.github.com>
2022-03-22 20:32:43 -0400

    Update Database.py

--- Database.py
+++ Database.py
@@ -4,80 +4,32 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import datetime as dt
-import math
 import sqlalchemy as sa
 import base64
-import sys
-
-def TotalTime(db):
-    x = []
-    y = []
-    for entry in db:
-        v = dt.datetime.fromtimestamp(db[entry]['integratedTime'])
-        x.append(dt.date(v.year, v.month, v.day))
-        y.append(len(x))
-        
-    i = int((365 * (x[-1].year - x[0].year) + 30 * (x[-1].month - x[0].month) + x[-1].day - x[0].day) / 5)
-    
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = i))
-    plt.plot(x, y)
-    plt.gcf().autofmt_xdate()
     
 def ChangeTime(db):
-    x = []
-    yaxis = []
-    for entry in db:
-        v = dt.datetime.fromtimestamp(db[entry]['integratedTime'])
-        x.append(dt.date(v.year, v.month, v.day))
-        yaxis.append(len(x))
-    
-    y = []
-    for k in x:
-        n = 1
-        result  = 0
-        for m in range(0, n + 1):
-            sign = (-1)**m
-            coefficient = math.comb(n, m)
-            entry = yaxis[-1 - m]
-            result += sign * coefficient * entry
-        y.append(result)
-        
-    i = int((365 * (x[-1].year - x[0].year) + 30 * (x[-1].month - x[0].month) + x[-1].day - x[0].day) / 5)
-    
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = i))
-    plt.plot(x, y)
-    plt.gcf().autofmt_xdate()
-    
-def DistTime(db):
-    formats = []
     xaxis = []
     for entry in db:
-        formats.append(db[entry]['body']['spec']['signature']['format'])
         v = dt.datetime.fromtimestamp(db[entry]['integratedTime'])
         xaxis.append(dt.date(v.year, v.month, v.day))
     
-    labels = []
-    for i in formats:
-        if i not in labels:
-            labels.append(i)
+    x = []
+    y = []
+    for j in xaxis:
+        if j not in x:
+            x.append(j)
+            y.append(xaxis.count(j))
+    
+    slope = [y[0]]
+    for k in range(len(x) - 1):
+        slope.append(y[k + 1] - y[k])
         
-    i = int((365 * (xaxis[-1].year - xaxis[0].year) + 30 * (xaxis[-1].month - xaxis[0].month) + xaxis[-1].day - xaxis[0].day) / 5)
-    colors = plt.cm.rainbow(np.linspace(0, 1, len(xaxis)))
+    i = int((365 * (x[-1].year - x[0].year) + 30 * (x[-1].month - x[0].month) + x[-1].day - x[0].day) / 5)
     
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
     plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = i))
-    for m in range(len(labels)):
-        x = []
-        y = []
-        for n in range(len(formats)):
-            if formats[n] == labels[m]:
-                x.append(xaxis[n])
-                y.append(len(x))
-        plt.plot(x, y, label = labels[m], color = colors[m])
+    plt.plot(x, slope)
     plt.gcf().autofmt_xdate()
-    plt.legend()
 
 def DistBar(db):
     formats = []
@@ -99,6 +51,35 @@ def DistBar(db):
     plt.xlabel('Count')
     plt.ylabel('Type')
     plt.show()
+
+def DistTime(db):
+    formats = []
+    xaxis = []
+    for entry in db:
+        formats.append(db[entry]['body']['spec']['signature']['format'])
+        v = dt.datetime.fromtimestamp(db[entry]['integratedTime'])
+        xaxis.append(dt.date(v.year, v.month, v.day))
+    
+    labels = []
+    for i in formats:
+        if i not in labels:
+            labels.append(i)
+        
+    i = int((365 * (xaxis[-1].year - xaxis[0].year) + 30 * (xaxis[-1].month - xaxis[0].month) + xaxis[-1].day - xaxis[0].day) / 5)
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(labels)))
+    
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = i))
+    for m in range(len(labels)):
+        x = []
+        y = []
+        for n in range(len(formats)):
+            if formats[n] == labels[m]:
+                x.append(xaxis[n])
+                y.append(len(x))
+        plt.plot(x, y, label = labels[m], color = colors[m])
+    plt.gcf().autofmt_xdate()
+    plt.legend()
     
 def Table(db):
     metadata = sa.MetaData()
@@ -107,7 +88,7 @@ def Table(db):
     xaxis = []
     for entry in db:
         formats.append(db[entry]['body']['spec']['signature']['format'])
-        v = dt.datetime.fromtimestamp(db[entry]['integratedTime'])
+        v = dt.datetime.fromtimestamp(db[entry]['IntegratedTime'])
         xaxis.append(dt.date(v.year, v.month, v.day))
     
     labels = []
@@ -134,6 +115,21 @@ def Table(db):
         sa.Column('time', sa.DateTime, nullable = False),
         sa.Column('count', sa.Integer, key = 'count'), 
         sa.Column('total', sa.Integer, nullable = False))
+    
+def TotalTime(db):
+    x = []
+    y = []
+    for entry in db:
+        v = dt.datetime.fromtimestamp(db[entry]['integratedTime'])
+        x.append(dt.date(v.year, v.month, v.day))
+        y.append(len(x))
+        
+    i = int((365 * (x[-1].year - x[0].year) + 30 * (x[-1].month - x[0].month) + x[-1].day - x[0].day) / 5)
+    
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = i))
+    plt.plot(x, y)
+    plt.gcf().autofmt_xdate()
 
 def main():
     con = sqlite3.connect('testy2.db')
@@ -142,24 +138,15 @@ def main():
     db = {}
     for row in cur:
         db[row[0]] = json.loads(row[1])
-    
+        
     for i in db:
         db[i]['body'] = json.loads(base64.b64decode(db[i]['body']))
     
-    # Table(db)
-    # DistBar(db)
-    # TotalTime(db)
-    # DistTime(db)
     # ChangeTime(db)
+    # DistBar(db)
+    # DistTime(db)
+    # Table(db)
+    # TotalTime(db)
                     
 if __name__ == '__main__':
     main()
-    
-    # target = sys.stdin.readline()
-    # target = sys.stdin.readline()
-
-    # while True:
-    #     target = sys.stdin.readline()
-    #     target = sys.stdin.readline()
-    #     payload = json.loads(target)
-    #     payload['body'] = json.loads(base64.b64decode(payload['body']))
