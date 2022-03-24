1092bd296206803b567c7c7e766a207e402a87ad
Sarah-Clevenger <89268804+Sarah-Clevenger@users.noreply.github.com>
2022-03-24 09:40:18 -0400

    Update ChangeTime.py

--- ChangeTime.py
+++ ChangeTime.py
@@ -19,9 +19,7 @@ def ChangeTime(db):
     for k in range(len(x) - 1):
         slope.append(y[k + 1] - y[k])
         
-    i = int((365 * (x[-1].year - x[0].year) + 30 * (x[-1].month - x[0].month) + x[-1].day - x[0].day) / 5)
-    
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = i))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = int((x[-1] - x[0]).days / 5)))
     plt.plot(x, slope)
     plt.gcf().autofmt_xdate()
