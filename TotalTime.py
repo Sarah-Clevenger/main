24b296746e85ac79c57f7d3742bc7340441a386f
Sarah-Clevenger <89268804+Sarah-Clevenger@users.noreply.github.com>
2022-04-13 19:40:48 -0400

    Update TotalTime.py

--- TotalTime.py
+++ TotalTime.py
@@ -9,8 +9,11 @@ def TotalTime(db):
         v = dt.datetime.fromtimestamp(db[entry]['integratedTime'])
         x.append(dt.date(v.year, v.month, v.day))
         y.append(len(x))
-        
+
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
     plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval = int((x[-1] - x[0]).days / 5)))
     plt.plot(x, y)
     plt.gcf().autofmt_xdate()
+    plt.title('Total Count Over Time Graph')
+    plt.xlabel('Time')
+    plt.ylabel('Count')
